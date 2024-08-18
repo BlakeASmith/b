00b17492999cb2b43fc03f01c1aea094112a3037
Blake Smith <blakeasm@amazon.com>
2024-08-18 13:29:01 -0700

    feat: add entrypoint for mac

--- ansible/roles/bash/files/bash_extensions/configure_direnv.sh
+++ ansible/roles/bash/files/bash_extensions/configure_direnv.sh
@@ -3,10 +3,10 @@ $(which direnv &>/dev/null) && eval "$(direnv hook bash)" &>/dev/null || return
 
 
 function mkvenv() {
-    python3 -m venv "$PWD/.venv"
-    echo "export VIRTUAL_ENV=$PWD/.venv" >> .envrc
-    echo "export PATH=$PWD/.venv/bin:$PATH" >> .envrc
-    direnv allow "$PWD"
+    python3 -m venv .venv
+    echo "export VIRTUAL_ENV=\"$PWD/.venv\"" >> .envrc
+    echo "export PATH=\"$PWD/.venv/bin:$PATH\"" >> .envrc
+    direnv allow .
 }
 
 
